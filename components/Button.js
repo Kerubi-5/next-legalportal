611ab8a3c695ee73b3721a16fe41e9d89731efc0
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-13 18:31:50 +0800

    adds outline

--- components/Button.js
+++ components/Button.js
@@ -5,6 +5,12 @@ const Button = ({ children, color, icon }) => {
     if (color === "info") return "text-cyan-100 bg-cyan-500 hover:bg-cyan-600";
     if (color === "success")
       return "text-green-100 bg-green-500 hover:bg-green-600";
+    if (color === "danger-outline")
+      return "text-rose-500 border border-rose-500 hover:bg-rose-600 hover:text-rose-100";
+    if (color === "info-outline")
+      return "text-cyan-500 border border-cyan-500 hover:bg-cyan-600 hover:text-cyan-100";
+    if (color === "success-outline")
+      return "text-green-500 border border-green-500 hover:bg-green-600 hover:text-green-100";
   };
   return (
     <button
