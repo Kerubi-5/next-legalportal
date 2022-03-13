8031a0f647a2b6bd5124d2f5206015ccd8554c68
John Kim Querobines <querobinesj@gmail.com>
2022-03-13 11:46:50 +0800

    adds footer section

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,17 @@
 import { UserProvider } from "@auth0/nextjs-auth0";
+import Footer from "./Footer";
 import Header from "./Header";
 
 const Layout = ({ children }) => {
   return (
     <UserProvider>
-      <Header />
-      <main className="container mx-auto px-4 dark font-poppins">
-        {children}
-      </main>
+      <div className="dark">
+        <Header />
+        <main className="dark:bg-slate-900 dark:text-zinc-50 mx-auto px-4 font-poppins">
+          {children}
+        </main>
+        <Footer />
+      </div>
     </UserProvider>
   );
 };
