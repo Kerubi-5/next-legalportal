547045460f24a909f9b5b655a9221d12c3d5b22a
John Kim A. Querobines <querobinesj@gmail.com>
2022-03-13 20:38:36 +0800

    changes semantic tag on small

--- components/Typography.js
+++ components/Typography.js
@@ -5,7 +5,10 @@ const Typography = ({ type, children }) => {
     return (
       <h3 className="text-lg md:text-xl dark:text-zinc-400">{children}</h3>
     );
-  if (type === "small") return <p className="text-xs md:text-sm">{children}</p>;
+  if (type === "small")
+    return (
+      <small className="text-xs text-gray-500 md:text-sm">{children}</small>
+    );
   return (
     <p className="text-justify text-gray-600 dark:text-zinc-500 text-sm md:text-base">
       {children}
